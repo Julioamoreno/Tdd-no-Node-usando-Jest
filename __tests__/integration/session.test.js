4f6614a80e325484cfe9696e6f12ccee6c9b3b27
Júlio Armando <julioarmando321@gmail.com>
2019-12-11 02:46:48 -0300

    Adicionando testes model usuario

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,15 +1,89 @@
-const {User} = require('../../src/app/models');
+const request = require('supertest');
+
+const app = require('../../src/app');
+const factory = require('../factories');
+const truncate = require('../utils/truncate');
 
 
 describe('Authentication',()=>{
-    it('should sum two numbers',async ()=>{
-     const user =  await User.create({name: 'julio',
-     email: 'teste@teste.com',
-     password_hash: '123123'
+    beforeEach(async()=>{
+        await truncate();
     });
-        console.log(user);
-        expect(user.email).toBe('teste@teste.com');
+
+    it('should authemticate with valid credentials', async ()=>{
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+        .post('/session')
+        .send({
+            email: user.email,
+            password: '123123'
+        });
+
+        expect(response.status).toBe(200);
     });
+
+
+    it('should not authenticate with invalid credentials', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+        .post('/session')
+        .send({
+            email: user.email,
+            password: '321321'
+        });
+
+        expect(response.status).toBe(401);
+    })
+
+    it('Deve retornar jwt token quando autenticado', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+        .post('/session')
+        .send({
+            email: user.email,
+            password: '123123'
+        });
+
+        expect(response.body).toHaveProperty('token')
+    })
+
+    it('Pode acessar rotas quando autenticado', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+        .get('/dashboard')
+        .set('Authorization', `Bearer ${user.generateToken()}`)
+
+
+        expect(response.status).toBe(200);
+    })
+
+    it('Não pode acessar rotas quando não autenticado', async () => {
+        const response = await request(app)
+        .get('/dashboard')
+
+        expect(response.status).toBe(401);
+    })
+
+    it('Não pode acessar rotar com um token jwt invalido', async() => {
+        const response = await request(app)
+        .get('/dashboard')
+        .set('Authorization', `Bearer 123123`)
+
+
+        expect(response.status).toBe(401);
+    })
 })
 
 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,16 @@
 const routes = require('express').Router();
 
+const authMiddleware = require('./app/middleware/auth');
 
+const SessionController = require("./app/controllers/SessionController");
+
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/dashboard', (req, res) => {
+    return res.status(200).send();
+})
 //Definição de rotas
 
 module.exports= routes;
